244e6d707082d695ad12f72b551baee4665b5062
agoever <128943290+agoever@users.noreply.github.com>
2026-03-06 14:13:12 +0800

    Clone luci-app-tailscale-community and modify menus
    Add luci-app-tailscale-community and update menu display settings.

--- diy-part5-6.6.sh
+++ diy-part5-6.6.sh
@@ -72,6 +72,8 @@ git clone https://github.com/modelsun/luci-app-pushbot.git package/luci-app-push
 git clone https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
 git clone https://github.com/timsaya/openwrt-bandix.git package/openwrt-bandix
 
+git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/luci-app-tailscale-community
+
 
 #修改显示位置
 sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
@@ -79,10 +81,9 @@ sed -i 's/Network Shares/Ksmbd共享/g' feeds/luci/applications/luci-app-ksmbd/r
 sed -i 's/services/nas/g' feeds/luci/applications/luci-app-transmission/root/usr/share/luci/menu.d/luci-app-transmission.json
 sed -i 's/services/nas/g' feeds/luci/applications/luci-app-qbittorrent/root/usr/share/luci/menu.d/luci-app-qbittorrent.json
 
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
 sed -i 's/services/network/g' feeds/luci/applications/luci-app-banip/root/usr/share/luci/menu.d/luci-app-banip.json
 sed -i 's/"title": "banIP"/"title": "BanIP"/g' feeds/luci/applications/luci-app-banip/root/usr/share/luci/menu.d/luci-app-banip.json
-
+sed -i 's/services/vpn/g' package/luci-app-tailscale-community/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
 
 # 添加组播防火墙规则
 cat >> package/network/config/firewall/files/firewall.config <<EOF
